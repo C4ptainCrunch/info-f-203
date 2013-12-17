79046ce87ee35d24df84715d8df2a325a2e52df1
Romain Fontaine <romain-fontaine@outlook.com>
2013-12-17 17:18:04 +0100

    add debt truc.

--- Debt.java
+++ Debt.java
@@ -3,11 +3,17 @@ public class Debt {
 	private Node from;
 	private Node to;
 	
-	public Debt(String from, String to, int amount){
-
+	public Debt(Node from, Node to, int amount){
+        from = from;
+        to = to;
+        amount = amount;
 	}
 
 	public Node getFrom() {
 		return from;
 	}
+
+    public static Debt fromInfo(Graph graph, String from, String to, int amount) {
+        return new Debt(graph.getNode(from), graph.getNode(to), amount);
+    }
 }
