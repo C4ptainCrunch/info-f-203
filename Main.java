4f506aadbdf26a192f1166c507b43a6fcdc0dadc
Romain Fontaine <romain-fontaine@outlook.com>
2013-12-19 16:19:55 +0100

    \n

--- Main.java
+++ Main.java
@@ -4,12 +4,13 @@ public class Main {
             String fileName = args[0];
             Graph graph = Graph.fromFile(fileName);
             graph.toFile("start.gv");
-            System.out.println("Cycles supprimés");
+            System.out.println("\nCycles supprimés");
             graph.cycleDetectAndResolve();
             graph.toFile("dettesNoCycles.gv");
             System.out.println("\nFichier dettesNoCycles.gv contient la situation sans cycles.\n" +
                 "Utilisez la commande : dot dettesNoCycles.gv -Tpng > noCycles.png" + 
                 " pour créer l’image.\n");
+            System.out.println("Ordre des remboursements :\n");
             graph.resolveDebt();
             graph.toFile("dettesRemb.gv");
             System.out.println("\nFichier dettesRemb.gv contient la situation actuelle.\n" +
